23bd3ec059e62f2a467411c57176d7dce1a9eb7c
Jason Edwards <jason@stuart-edwards.info>
2022-08-11 22:43:56 +0100

    Drawn orbits and styles

--- src/js/config.js
+++ src/js/config.js
@@ -1,17 +1,19 @@
 const planets = {
-    //       [Name,         size,  dist sol,  orbit,  x coord, y coord, radius, sAngle, eAngle, colour]
-    sun:     ["Sun",        15,    0,         0,      375,     375,     30,     0,      2,      "yellow"],
-    mercury: ["Mercury",    1,     2,         88,     340,     340,     6,      0,      2,      "silver"],
-    venus:   ["Venus",      2,     4,         225,    320,     320,     8,      0,      2,      "purple"],
-    earth:   ["Earth",      2.5,   6,         365,    300,     300,     10,     0,      2,      "green" ],
-    mars:    ["Mars",       1.25,  8,         687,    280,     280,     5,      0,      2,      "red"],
-    jupiter: ["Jupiter",    27.5,  10,        4331,   250,     250,     20,     0,      2,      "orange"],
-    saturn:  ["Saturn",     24.75, 12,        10747,  220,     220,     18,     0,      2,      "moccasin"],
-    uranus:  ["Uranus"],
-    neptune: ["Neptune"]
+    //       [Name,         size,  dist sol,  orbit,  x coord, y coord, radius, sAngle, eAngle, colour,     textColour]
+    sun:     ["Sun",        15,    0,         0,      375,     375,     30,     0,      2,      "yellow",   "black"],
+    mercury: ["Mercury",    1,     2,         88,     335,     375,     5,      0,      2,      "silver",   "yellow"],
+    venus:   ["Venus",      2,     4,         225,    305,     375,     10,     0,      2,      "purple",   "yellow"],
+    earth:   ["Earth",      2.5,   6,         365,    275,     375,     12,     0,      2,      "green",    "white"],
+    mars:    ["Mars",       1.25,  8,         687,    245,     375,     10,     0,      2,      "red",      "white"],
+    jupiter: ["Jupiter",    27.5,  10,        4331,   205,     375,     20,     0,      2,      "orange",   "black"],
+    saturn:  ["Saturn",     24.75, 12,        10747,  155,     375,     18,     0,      2,      "moccasin", "black"],
+    uranus:  ["Uranus",     0,     0,         0,      105,     375,     16,     0,      2,      "aqua",     "black"],
+    neptune: ["Neptune",    0,     0,         0,      55,      375,     12,     0,      2,      "blue",     "white"]
 };
 
 const properties = {
     canvasName: "orrery",
-    canvasContext: "2d"
+    canvasContext: "2d",
+    canvasWidth: 750,
+    canvasHeight: 750
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,62 @@
 
 function main() {
-    generateStars();
+    const c = initCanvas();
+
+    generateStars(c);
+
     for (let p in planets) {
-        drawPlanet(planets[p]);
+        drawOrbit(c, planets[p]);
+        drawPlanet(c, planets[p]);
     }
     infoTable();
 }
 
-function drawArc(x, y, r, sAngle, eAngle, colour, name) {
+function initCanvas () {
     const canvas = document.getElementById(properties.canvasName);
+    canvas.width = properties.canvasWidth;
+    canvas.height = properties.canvasHeight;
     var ctx = canvas.getContext(properties.canvasContext);
-    ctx.fillStyle = colour;
-    ctx.beginPath();
-    ctx.arc(x, y, r, sAngle, eAngle * Math.PI);
-    ctx.fill();
-    ctx.font = '20pt';
-    ctx.fillStyle = 'black';
-    ctx.textAlign = 'center';
-    ctx.fillText(name, x, y+4);
-
+    return ctx;
 }
 
-function drawPlanet(planet) {
-    drawArc(planet[4], planet[5], planet[6], planet[7], planet[8], planet[9], planet[0]);
-    if (planet[0] != "Sun") {
-        drawOrbit(planet[4], planet[5], ((planet[6])/(planets.sun[6])), planet[7], planet[8]);
+function generateStars(c) {
+    var x, y = Math.random();
+    var dia = c.canvas.clientWidth;
+
+    for(; y < dia; y++) {
+        for(x = Math.random(); x < dia; x++) {
+            c.fillStyle = "white";
+            c.fillRect(
+                (Math.floor(Math.random() * c.canvas.clientWidth)*(200* Math.random())), 
+                (Math.floor(Math.random() * c.canvas.clientHeight)*(150* Math.random())), 1, 1);
+        }
     }
 }
 
-function drawOrbit(x, y, r, sAngle, eAngle) {
-    const canvas = document.getElementById(properties.canvasName);
-    var ctx = canvas.getContext(properties.canvasContext);
-    ctx.beginPath();
-    ctx.arc(x, y, r, sAngle, eAngle * Math.PI);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "white";
-    ctx.stroke();
+function drawArc(c, x, y, r, sAngle, eAngle, colour, name, textColour) {
+    c.fillStyle = colour;
+    c.beginPath();
+    c.arc(x, y, r, sAngle, eAngle * Math.PI);
+    c.fill();
+    c.font = '20pt';
+    c.fillStyle = textColour;
+    c.textAlign = 'center';
+    c.fillText(name, x, y);
 
+}
+
+function drawPlanet(c, planet) {
+    drawArc(c, planet[4], planet[5], planet[6], planet[7], planet[8], planet[9], planet[0], planet[10]);
+}
+
+function drawOrbit(c, planet) {
+    if (planet[0] !='Sun') {
+        c.beginPath();
+        c.arc(planets.sun[4], planets.sun[5], planets.sun[6]+((planet[5]-planet[4])-planet[6])-(planets.sun[6]-planet[6]), 0, 2 * Math.PI);
+        c.lineWidth = 1;
+        c.strokeStyle = "white";
+        c.stroke();
+    }
 }
 
 function infoTable() {
@@ -59,19 +79,5 @@ function infoTable() {
            "<td>"+planets[p][3]+"</td>" +
            "<td>"+planets[p][9]+"</td>" + 
         "</tr>";
-    }
-}
-
-function generateStars() {
-    const canvas = document.getElementById(properties.canvasName);
-    var ctx = canvas.getContext(properties.canvasContext);
-    var x, y = Math.random();
-    var dia = canvas.width;
-
-    for(; y < dia; y++) {                           // walk x/y grid
-        for(x = Math.random(); x < dia; x++) {
-            ctx.fillStyle = "white";          // set random color
-            ctx.fillRect((Math.floor(Math.random() * canvas.width)*(200* Math.random())), (Math.floor(Math.random() * canvas.height)*(150* Math.random())), 1, 1);               // draw a pixel
-        }
     }
 }
